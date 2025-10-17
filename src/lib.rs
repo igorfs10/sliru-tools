e8d6be0ac69a8623b1789a913ee29f6d8d6d63c5
igorfs10 <igorfs10@gmail.com>
2025-10-17 19:04:35 -0300

    progresso

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
 slint::include_modules!();
 
+
+// fn abrir_nova_janela() {
+//     let nova_janela = AppWindow::new().unwrap();
+//     // nova_janela.run().unwrap();
+//     nova_janela.show().unwrap();
+// }
+
 #[cfg(not(target_arch = "wasm32"))]
 pub fn start_desktop() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    // ui.on_request_increase_value({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
 
     ui.run()?;
 
